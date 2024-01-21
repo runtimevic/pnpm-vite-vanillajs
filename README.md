7aa554d6c70f07c90f9a5a5edd2c9ae019fb596d
runtimevic <victorduranm@hotmail.com>
2024-01-21 14:22:56 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+ ```pnpm create vite```
+ 
+cd pnpm-vite-vanillajs
+pnpm install   
+pnpm run dev 
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+
+
+export default defineConfig({
+    base: '/pnpm-vite-vanillajs/',
+    plugins: []
+});
